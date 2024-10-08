06a9b66034596bea3c3b9173522a35a20d0f644d
smahdianipur0 <157228225+smahdianipur0@users.noreply.github.com>
2024-10-08 23:44:07 +0330

    clean-up

---
+++ src/components/SWRMutation.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { trpcAstroClient } from "../client";
+import useSWRMutation from "swr/mutation";
+
+function Profile() {
+  const [name, setName] = useState("OldMate");
+
+  const updateUser = async (newData) => {
+    const response = await trpcAstroClient.greetWithName.mutate({
+      names: newData,
+    });
+    return response;
+  };
+
+  const { data, error, trigger, isMutating } = useSWRMutation(name, updateUser);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button
+        disabled={isMutating || name === ""}
+        onClick={async () => {
+          await trigger(name);
+          setName("");
+        }}
+      >
+        Greet
+      </button>
+      {error && <div>Error loading greeting.</div>}
+      {isMutating && <div>Loading...</div>}
+      {!isMutating && data && <div>{data.message}</div>}
+    </div>
+  );
+}
+
+export default Profile;
